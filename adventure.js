4291f11ecf60c680e744498c6c9c0634e8d18987
Bartek <99047458@mydavinci.nl>
2019-12-04 11:49:28 +0100

    Wat ik tot nu toe heb gedaan:

--- adventure.js
+++ adventure.js
@@ -5,6 +5,7 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var inventoryItem = document.getElementById("inventoryItem");
+var inventoryItem2 = document.getElementById("inventoryItem2");
 
 var fuelCanister = false;
 var shovel = false;
@@ -12,6 +13,7 @@ var shovel = false;
 function startGame(){
 	title.innerHTML = "Escape from the Murderer";
 	inventoryItem.style.display ="none"
+	inventoryItem2.style.display ="none"
 	description.innerHTML = "You have landed in a forest deep in the night with a serial killer. You need to escape and just.. don't let him kill you.";
 	button1.innerHTML = "CREDITS"
 	button1.setAttribute("onclick", "credits()");
@@ -34,6 +36,7 @@ function beginning(){
 	description.innerHTML = "[FOREST] There is a small house on the left and you can follow the path on the right.";
     button1.innerHTML = "EXPLORE THE HOUSE";
     button1.setAttribute("onclick", "house()");
+    button2.style.display = "inline-block";
     button2.innerHTML = "FOLLOW THE PATH";
     button2.setAttribute("onclick", "path();");
     button3.style.display = "none";
@@ -52,9 +55,11 @@ function house(){
 function house1(){
 	image.src ="dead3.jpg";
 	description.innerHTML = "THE MURDERER FOUND YOU...YOU ARE DEAD!";
-	button1.style.display = "none";
+	button1.innerHTML = "TRY AGAIN";
+	button1.setAttribute("onclick", "window.location.reload()");
     button2.style.display = "none";
     button3.style.display = "none";
+
 }
 
 function path(){
@@ -72,6 +77,7 @@ function path(){
 function camping(){
 	image.src ="camping.jpg"
 	description.innerHTML ="[CAMPING] You arrived at a camping. What do you want to do?"
+	button1.style.display = "inline-block";
 	button1.innerHTML = "HIDE IN THE TENT";
     button1.setAttribute("onclick", "dead1()");
     button2.innerHTML = "GO FURTHER";
@@ -83,14 +89,17 @@ function camping(){
 function dead1(){
 	image.src ="dead2.jpg"
 	description.innerHTML ="THAT WAS NOT THE BEST IDEA..HE FOUND YOU AND YES... YOU ARE DEAD!";
-	button1.style.display = "none";
+	button1.innerHTML = "TRY AGAIN";
+	button1.setAttribute("onclick", "window.location.reload()");
 	button2.style.display = "none";
 	button3.style.display = "none";
+	
 }
 
 function house3(){
 	image.src ="house4.jpg"
 	description.innerHTML ="[IN FRONT OF THE HOUSE]You found another house, do you want to explore it?";
+	button1.style.display ="inline-block";
 	button1.innerHTML = "EXPLORE THE HOUSE";
     button1.setAttribute("onclick", "exploring()");
     button2.innerHTML = "GO BACK";
@@ -99,13 +108,28 @@ function house3(){
 }
 
 function exploring(){
-	image.src ="fuelhouse.jpg"
-	description.innerHTML ="[IN THE HOUSE]Look around, maybe you will find something usefull..."
-	button1.innerHTML = "LOOK AROUND";
-    button1.setAttribute("onclick", "fuelCanister()");
+	if(fuelCanister == false){
+	  image.src ="fuelhouse.jpg"
+	  description.innerHTML ="[IN THE HOUSE]Look around, maybe you will find something usefull..."
+	  button1.innerHTML = "LOOK AROUND";
+	  button1.setAttribute("onclick", "getFuel()");
+	  button1.style.display ="inline-block";
+  } else {
+	  image.src ="house3.jpg"
+      button1.style.display = "none";
+      inventoryItem.style.display = "inline-block";
+      inventoryItem.src = "canister.jpg"
+      inventoryItem.style.width = "100px";
+      description.innerHTML ="[IN THE HOUSE]Looks like you found a fuel canister and it seems to be full!"
+	}
     button2.innerHTML = "LEAVE THE HOUSE";
     button2.setAttribute("onclick", "house3()");
     button3.style.display = "none";
+ }
+
+function getFuel(){
+	fuelCanister = true;
+	exploring();
 }
 
 function forest2(){
@@ -115,41 +139,80 @@ function forest2(){
     button1.setAttribute("onclick", "graveyard()");
     button2.innerHTML = "MOVE FURTHER";
     button2.setAttribute("onclick", "car()");
+    button3.style.display = "inline-block";
     button3.innerHTML = "GO BACK";
     button3.setAttribute("onclick", "camping()");
 }
 
 function graveyard(){
-	image.src ="graveyard.jpg"
-	description.innerHTML ="[GRAVEYARD] It's creapy here... What do you want to do?"
-	button1.innerHTML = "LOOK AROUND";
-    button1.setAttribute("onclick", "shovel()");
+    if(shovel == false){
+	  image.src ="shovelgraveyard.jpg"
+	  description.innerHTML ="[GRAVEYARD] It's creapy here... What do you want to do?"
+	  button1.innerHTML = "LOOK AROUND";
+	  button1.setAttribute("onclick", "getShovel()");
+	  button1.style.display ="inline-block";
+   } else {
+	  image.src ="graveyard.jpg"
+      button1.style.display = "none";
+      inventoryItem2.style.display = "inline-block";
+      inventoryItem2.src = "shovel.jpg"
+      inventoryItem2.style.width = "100px";
+      description.innerHTML ="[GRAVEYARD]You found a shovel! There is nothing usefull anymore...!"
+	}
     button2.innerHTML = "GO BACK";
     button2.setAttribute("onclick", "hello()");
     button3.style.display = "none";
+ }
+
+ function getShovel(){
+	shovel = true;
+	graveyard();
 }
 
 function hello(){
 	image.src ="hellothere.jpg"
 	description.innerHTML ="HE FOUND YOU! WHAT NOW???"
+	button1.style.display ="inline-block";
 	button1.innerHTML = "TRY TO RUN";
     button1.setAttribute("onclick", "killed()");
     button2.innerHTML = "TRY TO HIT HIM WITH THE SHOVEL";
-    button2.setAttribute("onclick", "shovel()");
+    button2.setAttribute("onclick", "useShovel()");
     button3.style.display = "none";
 }
+
+function useShovel(){
+    if(shovel == false){
+	  alert("YOU DON'T HAVE A SHOVEL!"); 
+	  button2.style.display = "none";
+	
+   } else {
+	  image.src ="forest2.jpg"
+      button1.style.display = "none";
+      inventoryItem2.style.display = "none";
+      description.innerHTML ="[NEAR THE GRAVEYARD]You have escaped from him for now..You should be fast and look for escape!"
+      button3.style.display ="inline-block";
+      button3.innerHTML = "GO BACK";
+      button3.setAttribute("onclick", "camping()");
+	}
+      button2.innerHTML = "MOVE FURTHER";
+      button2.setAttribute("onclick", "car()");
+      
+ }
 
 function killed(){
 	image.src ="killed.jpg"
 	description.innerHTML ="HE CATCHED YOU..I AM SORRY BUT.. YOU'RE DEAD!"
-	button1.style.display = "none";
+	button1.innerHTML = "TRY AGAIN";
+	button1.setAttribute("onclick", "window.location.reload()");
 	button2.style.display = "none";
 	button3.style.display = "none";
+	
 }
 
 function car(){
 	image.src ="car.jpg"
 	description.innerHTML ="[CAR] You found a car. Maybe it's your way out of here!"
+	button1.style.display ="inline-block";
 	button1.innerHTML = "GET IN THE CAR";
     button1.setAttribute("onclick", "car2()");
     button2.innerHTML = "GO BACK";
@@ -158,13 +221,25 @@ function car(){
 }
 
 function car2(){
-	image.src ="insidecar.jpg"
-	description.innerHTML ="[INSIDE-CAR] Looks like the car isn't damaged, but it's out of fuel.. You need to find a fuel canister to start the car and escape!!"
-	button1.innerHTML = "GET OUT OF THE CAR";
-    button1.setAttribute("onclick", "car()");
-    button2.innerHTML = "REFUEL THE CAR";
-    button2.setAttribute("onclick", "refuel()");
+	if(fuelCanister == false){
+	  image.src ="insidecar.jpg"
+	  description.innerHTML ="[INSIDE-CAR] Looks like the car isn't damaged, but it's out of fuel.. You need to find a fuel canister to start the car and escape!!"
+	  button1.style.display = "none";
+  } else {
+	  image.src ="movecar.jpg"
+      button1.style.display = "none";
+      inventoryItem.style.display = "none";
+      description.innerHTML ="[INSIDE CAR]CONGRATULATIONS! YOU HAVE ESCAPED!!"
+      button2.style.display ="none";
+	}
+    button2.innerHTML = "GET OUT OF THE CAR";
+    button2.setAttribute("onclick", "car()");
     button3.style.display = "none";
+ }
+
+function getFuel(){
+	fuelCanister = true;
+	exploring();
 }
 
 startGame();
